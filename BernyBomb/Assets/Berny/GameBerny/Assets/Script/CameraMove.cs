494ae9ae96ca24ed96179aee97f6b85de3bb33b7
Irene Checo <37472393+irenecheco@users.noreply.github.com>
2020-03-05 13:35:45 +0100

    Camera sotto menù sistemata

--- BernyBomb/Assets/Berny/GameBerny/Assets/Script/CameraMove.cs
+++ BernyBomb/Assets/Berny/GameBerny/Assets/Script/CameraMove.cs
@@ -44,46 +44,50 @@ public class CameraMove : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        //get the x position on the  mouse A rotate the target
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.Rotate(0,horizontal,0);
-        //get the y position of mouse and rotate pivot
-        float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
-        pivot.Rotate(-vertical, 0, 0);
-        if(invertY)
+        if (Time.deltaTime != 0f)
         {
-            pivot.Rotate(vertical, 0, 0);
-        } else
-        {
-            pivot.Rotate(-vertical, 0, 0); 
+            //get the x position on the  mouse A rotate the target
+            float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+            target.Rotate(0, horizontal, 0);
+            //get the y position of mouse and rotate pivot
+            float vertical = Input.GetAxis("Mouse Y") * rotateSpeed;
+            pivot.Rotate(-vertical, 0, 0);
+            if (invertY)
+            {
+                pivot.Rotate(vertical, 0, 0);
+            }
+            else
+            {
+                pivot.Rotate(-vertical, 0, 0);
+            }
+
+            //Limit up/down camera rotation
+            if (pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x < 180f)
+            {
+                pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
+            }
+            if (pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
+            {
+                pivot.rotation = Quaternion.Euler(360f + minViewAngle, 0, 0);
+            }
+
+            //move the camera based on the current rotation of the target and offset
+            float desireYangle = target.eulerAngles.y;
+            float desireXangle = pivot.eulerAngles.x;
+
+            Quaternion rotation = Quaternion.Euler(desireXangle, desireYangle, 0);
+            transform.position = target.position - (rotation * offset);
+
+
+            //transform.position = target.position - offset;
+
+            if (transform.position.y < target.position.y)
+            {
+                transform.position = new Vector3(transform.position.x, target.position.y - .5f, transform.position.z);
+            }
+            transform.LookAt(target);
+            ViewObstructed();
         }
-
-        //Limit up/down camera rotation
-        if(pivot.rotation.eulerAngles.x > maxViewAngle && pivot.rotation.eulerAngles.x <180f)
-        {
-            pivot.rotation = Quaternion.Euler(maxViewAngle, 0, 0);
-        }
-        if(pivot.rotation.eulerAngles.x > 180f && pivot.rotation.eulerAngles.x < 360f + minViewAngle)
-        {
-            pivot.rotation = Quaternion.Euler(360f + minViewAngle , 0, 0);
-        }
-
-        //move the camera based on the current rotation of the target and offset
-        float desireYangle = target.eulerAngles.y;
-        float desireXangle = pivot.eulerAngles.x;
-
-        Quaternion rotation = Quaternion.Euler(desireXangle, desireYangle, 0);
-        transform.position = target.position - (rotation * offset);
-
-
-        //transform.position = target.position - offset;
-
-        if(transform.position.y < target.position.y)
-        {
-            transform.position = new Vector3(transform.position.x, target.position.y -.5f, transform.position.z);
-        }
-        transform.LookAt(target);
-        ViewObstructed();
     }
 
     public void fakeTarget(GameObject newtarget)
